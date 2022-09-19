6a1d48d31166bb8d72720743397a90e77cc3af46
Haidar113 <113907088+Haidar113@users.noreply.github.com>
2022-09-19 14:20:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hansi vennerne!");
+        System.out.println("Commit mvh Haidar haha kh bent");
     }
 }
