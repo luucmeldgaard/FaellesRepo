2219802d84b409870d61d67212245ed65a50cc05
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-19 14:27:26 +0200

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hansi vennerne!");
+        System.out.println("Hansi");
         System.out.println("Commit mvh Haidar haha kh bent");
         System.out.println("Hey gutter!");
     }
