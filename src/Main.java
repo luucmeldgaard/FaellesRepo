3cf32c52af44e6c7745b2fe6146b9da2076221fc
luucmeldgaard <luucmeldgaard@outlook.com>
2022-09-19 14:43:41 +0200

    ENA ER FOR VILD

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hansi");
+        System.out.println("Ena");
         System.out.println("Commit mvh Haidar haha kh bent");
         System.out.println("Hey gutter!");
     }
